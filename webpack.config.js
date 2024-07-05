40e75c824779eec2d239e88dfee2310302803d4b
riley-kyule <phillipkyule@gmail.com>
2024-07-05 16:46:38 +0300

    webpack v4

--- webpack.config.js
+++ webpack.config.js
@@ -1,42 +1,44 @@
 const path = require('path');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: 'development',
-  entry: './src/index.js',
+  entry: './src/index.js', // Assuming this is your entry point for React
   devtool: 'inline-source-map',
   plugins: [
     new CleanWebpackPlugin(),
-    new HtmlWebpackPlugin({
-      template: './src/index.html'
-    }),
   ],
   output: {
-    filename: 'main.js',
+    filename: 'bundle.js', // Output bundle file name
     path: path.resolve(__dirname, 'dist'),
+    publicPath: '/', // Public path for assets
   },
   module: {
     rules: [
       {
+        test: /\.(js|jsx)$/, // Handle both .js and .jsx files
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader', // Use babel-loader for transpiling JSX
+          options: {
+            presets: ['@babel/preset-react'], // Presets for React
+          },
+        },
+      },
+      {
         test: /\.css$/,
-        use: [
-          'style-loader',
-          'css-loader',
-        ],
+        use: ['style-loader', 'css-loader'],
       },
       {
         test: /\.(png|svg|jpg|gif)$/,
-        type: 'asset/resource',
+        use: ['file-loader'],
       },
     ],
   },
   devServer: {
-    static: {
-      directory: path.resolve(__dirname, 'dist'),
-    },
+    contentBase: './dist', // Serve content from this directory
     compress: true,
-    port: 9000,
-    open: true,
+    port: 3000, // Port for localhost
+    historyApiFallback: true, // Handle React routing
   },
 };
